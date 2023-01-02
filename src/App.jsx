f92da1d364a117e503b8774d9e19dd86a9775835
sergiotto <sergiotto@outlook.com>
2023-01-02 14:12:12 +0100

    fixing localstorare

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,15 @@ import ShowListTasks from "./components/ShowListTasks";
 
 function App() {
   let tasksLS = JSON.parse(localStorage.getItem("tasks"));
+
   if (!tasksLS) {
     tasksLS = [];
   }
-  const [tasksList, setTasksList] = useState([]);
-
-  const fakeTasks = [
-    { id: "0001", name: "Limpiar la casa", complete: false },
-    { id: "0002", name: "Hacer la compra", complete: false },
-    { id: "0003", name: "Dar de comer a los gatos", complete: false },
-  ];
+  const [tasksList, setTasksList] = useState(tasksLS);
 
   useEffect(() => {
-    if (tasksLS) {
-      localStorage.setItem("tasks", JSON.stringify(tasksList));
-    } else {
-      localStorage.setItem("tasks"), JSON.stringify([]);
-    }
-  }, [tasksLS, tasksList]);
+    localStorage.setItem("tasks", JSON.stringify(tasksList));
+  }, [tasksList]);
 
   return (
     <div className="App">
@@ -30,7 +21,7 @@ function App() {
 
       <div className="container m-auto mt-5">
         <div className="my-5">
-          <FormNewTask tasksList={tasksList} setTasksList={setTasksList}/>
+          <FormNewTask tasksList={tasksList} setTasksList={setTasksList} />
         </div>
 
         <ShowListTasks tasksList={tasksList} />
